563be6605478c9b72d12cb7664c5320e9afe93b8
jeb47 <73932821+jebediah47@users.noreply.github.com>
2021-07-05 22:14:02 +0300

    Created translate command!

---
+++ src/commands/fun/translate.ts
@@ -0,0 +1,28 @@
+import translate = require("@iamtraction/google-translate");
+import { Command } from "../../interfaces";
+import { MessageEmbed } from "discord.js";
+
+export const command: Command = {
+  name: "translate",
+  aliases: ["trans", "langtrans", "google-translate"],
+  run: async (client, message, args) => {
+    const query = args.join(" ");
+    const embed2 = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("**Something went wrong...**")
+      .setDescription(
+        "Please specify a text to translate like the example below: \n `!translate {text}`"
+      )
+      .setTimestamp();
+
+    if (!query) return message.reply(embed2);
+    const translated = await translate(query, { to: "en" });
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("**This Translates to:**")
+      .setDescription(translated.text)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  },
+};
