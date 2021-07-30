0ea033e27902d1179601afe0b18248d0852cea9a
Christian Llupo <73932821+jebediah47@users.noreply.github.com>
2021-07-30 20:43:30 +0300

    Added ethereum command
    This command shows you the price of ethereum (a alternative for the crypto command)

---
+++ src/commands/crypto/ethereum.ts
@@ -0,0 +1,84 @@
+import { Command } from "../../interfaces";
+import { MessageEmbed } from "discord.js";
+import axios from "axios";
+
+export const command: Command = {
+  name: "ethereum",
+  aliases: ["eth", "ETHEREUM", "ETH"],
+  run: async (client, message, args) => {
+    let ethereum: string = `ethereum`;
+    let irl_currency: string = args[0];
+
+    const errEmbed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Notice!")
+      .setDescription(
+        "You need to add the full name of the cryptocurrency and your currency id e.x. \n" +
+          `\`\`\`${client.config.PREFIX}price bitcoin usd\`\`\` \n` +
+          "And here is a list of all the available cryptos/tokens and currency codes \n https://www.coingecko.com"
+      );
+
+    if (irl_currency === undefined) {
+      irl_currency = "usd";
+    } else {
+      console.log((error) => error);
+    }
+    try {
+      ethereum = encodeURIComponent(ethereum).toLowerCase();
+      irl_currency = encodeURIComponent(irl_currency).toLowerCase();
+      const { data } = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${ethereum}&vs_currencies=${irl_currency}&include_24hr_change=true`
+      );
+
+      if (data[ethereum][irl_currency] === undefined) {
+        return message.channel.send(
+          new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("We are sorry but...")
+            .setDescription(
+              "We couldn't find your currency code on the list here are the \n supported currencies by the [CoinGecko API](https://api.coingecko.com/api/v3/simple/supported_vs_currencies)"
+            )
+            .setThumbnail(
+              `https://static.coingecko.com/s/thumbnail-007177f3eca19695592f0b8b0eabbdae282b54154e1be912285c9034ea6cbaf2.png`
+            )
+            .setTimestamp()
+        );
+      }
+
+      function capitalize(s) {
+        if (typeof s !== "string") return "";
+        return s.charAt(0).toUpperCase() + s.slice(1);
+      }
+
+      function commaFormatter(x) {
+        var parts = x.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return parts.join(".");
+      }
+
+      const regularToken = `${data[ethereum][irl_currency]}`;
+      const change = `${data[ethereum].usd_24h_change}`.substring(
+        0,
+        `${data[ethereum].usd_24h_change}`.length - 13
+      );
+      return message.channel.send(
+        new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle(
+            capitalize(ethereum) + " " + irl_currency.toUpperCase() + " price!"
+          )
+          .setDescription(
+            `**Price:** \`${commaFormatter(regularToken)}\` ` +
+              irl_currency.toUpperCase() +
+              `\n **(24hr) Change:** \`${change}%\``
+          )
+          .setThumbnail(
+            `https://www.azcryptoexchanges.com/wp-content/uploads/2018/02/ethereum-1.png`
+          )
+          .setTimestamp()
+      );
+    } catch (err) {
+      return message.channel.send({ embed: errEmbed });
+    }
+  },
+};
