043d0e98b4adc93599ea7a5201c0ef34a44b8c71
Christian Llupo <73932821+jebediah47@users.noreply.github.com>
2021-07-30 20:23:51 +0300

    Fixed small embed issues for upcoming DJS v13

--- src/commands/crypto/crypto.ts
+++ src/commands/crypto/crypto.ts
@@ -19,43 +19,43 @@ export const command: Command = {
       );
 
     if (token === undefined) {
-      return message.channel.send(errEmbed);
+      return message.channel.send({ embed: errEmbed });
     } else if (irl_currency === undefined) {
       irl_currency = "usd";
     } else {
       console.log((err) => err);
     }
 
-    token = encodeURIComponent(token).toLowerCase();
-    irl_currency = encodeURIComponent(irl_currency).toLowerCase();
-    const { data } = await axios.get(
-      `https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=${irl_currency}&include_market_cap=true&include_24hr_change=true`
-    );
-
-    if (!data) {
-      return message.channel.send(
-        new MessageEmbed()
-          .setColor("RANDOM")
-          .setTitle("We are sorry but...")
-          .setDescription(
-            "We couldn't fetch the required data from the API, maybe try executing the command again!"
-          )
-          .setTimestamp()
-      );
-    }
-
-    function capitalize(s) {
-      if (typeof s !== "string") return "";
-      return s.charAt(0).toUpperCase() + s.slice(1);
-    }
-
-    function commaFormatter(x) {
-      var parts = x.toString().split(".");
-      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      return parts.join(".");
-    }
-
     try {
+      token = encodeURIComponent(token).toLowerCase();
+      irl_currency = encodeURIComponent(irl_currency).toLowerCase();
+      const { data } = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=${irl_currency}&include_market_cap=true&include_24hr_change=true`
+      );
+
+      if (!data) {
+        return message.channel.send(
+          new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("We are sorry but...")
+            .setDescription(
+              "We couldn't fetch the required data from the API, maybe try executing the command again!"
+            )
+            .setTimestamp()
+        );
+      }
+
+      function capitalize(s) {
+        if (typeof s !== "string") return "";
+        return s.charAt(0).toUpperCase() + s.slice(1);
+      }
+
+      function commaFormatter(x) {
+        var parts = x.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return parts.join(".");
+      }
+
       const regularToken = `${data[token][irl_currency]}`;
       const change = `${data[token].usd_24h_change}`.substring(
         0,
@@ -75,7 +75,7 @@ export const command: Command = {
           .setTimestamp()
       );
     } catch (err) {
-      return message.channel.send(errEmbed);
+      return message.channel.send({ embed: errEmbed });
     }
   },
 };
