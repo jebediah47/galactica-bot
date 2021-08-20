6b97643d3088ca41a54488bf4897c1c83bd4273a
Christian Llupo <pitagirokotopoulo@gmail.com>
2021-08-20 09:23:38 +0300

    Changed the vars from let to const on meme command

--- src/commands/fun/meme.ts
+++ src/commands/fun/meme.ts
@@ -8,14 +8,14 @@ export const command: Command = {
   run: (client, message) => {
     const embed = new MessageEmbed();
     got("https://www.reddit.com/r/memes/random/.json").then((response) => {
-      let content = JSON.parse(response.body);
-      let permalink = content[0].data.children[0].data.permalink;
-      let memeUrl = `https://reddit.com${permalink}`;
-      let memeImage = content[0].data.children[0].data.url;
-      let memeTitle = content[0].data.children[0].data.title;
-      let memeUpvotes = content[0].data.children[0].data.ups;
-      let memeDownvotes = content[0].data.children[0].data.downs;
-      let memeNumComments = content[0].data.children[0].data.num_comments;
+      const content = JSON.parse(response.body);
+      const permalink = content[0].data.children[0].data.permalink;
+      const memeUrl = `https://reddit.com${permalink}`;
+      const memeImage = content[0].data.children[0].data.url;
+      const memeTitle = content[0].data.children[0].data.title;
+      const memeUpvotes = content[0].data.children[0].data.ups;
+      const memeDownvotes = content[0].data.children[0].data.downs;
+      const memeNumComments = content[0].data.children[0].data.num_comments;
       embed.setTitle(`${memeTitle}`);
       embed.setURL(`${memeUrl}`);
       embed.setImage(memeImage);
