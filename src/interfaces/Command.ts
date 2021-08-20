9ef4d853daa68e836854acabab369482e4995cd2
Christian Llupo <pitagirokotopoulo@gmail.com>
2021-08-20 10:27:55 +0300

    Formatted Command.ts interface

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -2,12 +2,12 @@ import Client from "../client";
 import { Message } from "discord.js";
 
 interface Run {
-    (client: Client, message: Message, args: any[])
+  (client: Client, message: Message, args: any[]);
 }
 
 export interface Command {
-    name: string;
-    description?: string;
-    aliases?: string[];
-    run: Run;
+  name: string;
+  description?: string;
+  aliases?: string[];
+  run: Run;
 }
