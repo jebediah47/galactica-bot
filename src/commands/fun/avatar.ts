88d66d4955794073d2cef3bfd6cf9a4b22b6bf90
Christian Llupo <pitagirokotopoulo@gmail.com>
2021-08-20 09:01:34 +0300

    Removed unused args parameter on avatar command

--- src/commands/fun/avatar.ts
+++ src/commands/fun/avatar.ts
@@ -4,7 +4,7 @@ import { MessageEmbed } from "discord.js";
 export const command: Command = {
   name: "avatar",
   aliases: ["pfp"],
-  run: (client, message, args) => {
+  run: (client, message) => {
     const user = message.mentions.users.first() || message.author;
     message.channel.send(
       new MessageEmbed()
