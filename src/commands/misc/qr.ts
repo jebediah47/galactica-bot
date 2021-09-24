21f8dee75cb20573224a790393bfc2878cb755b8
Christian Llupo <christianllupo@protonmail.com>
2021-09-24 22:20:24 +0300

    Added qr command

---
+++ src/commands/misc/qr.ts
@@ -0,0 +1,32 @@
+import { Command } from "../../interfaces";
+import { MessageEmbed } from "discord.js";
+
+export const command: Command = {
+  name: "qr",
+  aliases: ["qrcode", "qr-code"],
+  run: async (client, message, args) => {
+    const text = args.join(" ");
+    if (!text) {
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("Notice!")
+        .setDescription(
+          "You need to add a text input so it can be converted to a QR-code"
+        )
+        .setTimestamp();
+      return message.channel.send({ embeds: [embed] });
+    }
+
+    const pleaseWait = message.channel.send(
+      "Please wait while your text is converted to QR-code"
+    );
+
+    const embed1 = new MessageEmbed()
+      .setImage(
+        `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
+      )
+      .setColor("RANDOM")
+      .setTimestamp();
+    (await pleaseWait).edit({ embeds: [embed1] });
+  },
+};
