12dd26f19c662affb549b4ebff7986bc3f98b0ed
Paleroukos <79189271+Paleroukos@users.noreply.github.com>
2021-09-27 12:02:04 +0300

    Update index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,2 +1,16 @@
-export { commaFormatter } from "./commaFormatter";
-export { capitalize } from "./capitalize";
+const commaFormatter = (x) => {
+    const parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+  };
+
+const capitalize = (s) => {
+    if (typeof s !== "string") return "";
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
+
+
+
+export default{
+    commaFormatter, capitalize
+}
