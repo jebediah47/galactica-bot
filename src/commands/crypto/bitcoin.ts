93e52c9043f80792c064334e12c89840bbfd06d9
Paleroukos <79189271+Paleroukos@users.noreply.github.com>
2021-09-27 12:02:43 +0300

    Update bitcoin.ts

--- src/commands/crypto/bitcoin.ts
+++ src/commands/crypto/bitcoin.ts
@@ -1,4 +1,4 @@
-import { capitalize, commaFormatter } from "../../functions";
+import functions from "../../functions";
 import { Command } from "../../interfaces";
 import { MessageEmbed } from "discord.js";
 import axios from "axios";
@@ -48,10 +48,10 @@ export const command: Command = {
       const embed2 = new MessageEmbed()
         .setColor("RANDOM")
         .setTitle(
-          capitalize(bitcoin) + " " + irl_currency.toUpperCase() + " price!"
+          functions.capitalize(bitcoin) + " " + irl_currency.toUpperCase() + " price!"
         )
         .setDescription(
-          `**Price:** \`${commaFormatter(regularToken)}\` ` +
+          `**Price:** \`${functions.commaFormatter(regularToken)}\` ` +
             irl_currency.toUpperCase() +
             `\n **(24hr) Change:** \`${change}%\` \n` +
             `*Powered by CoinGecko API*`
