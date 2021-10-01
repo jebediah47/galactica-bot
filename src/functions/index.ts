1fa78f2c0fe532cbaee23891554376d4c789cafd
Christian Llupo <christianllupo@protonmail.com>
2021-10-01 18:36:10 +0300

    Updated functions system

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,16 +1,10 @@
-const commaFormatter = (x) => {
-    const parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join(".");
-  };
+export function commaFormatter(x) {
+  const parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+}
 
-const capitalize = (s) => {
-    if (typeof s !== "string") return "";
-    return s.charAt(0).toUpperCase() + s.slice(1);
-  };
-
-
-
-export default{
-    commaFormatter, capitalize
+export function capitalize(s) {
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
 }
