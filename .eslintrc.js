feb64d474fec5503e508f93f2bdcac250f2cc099
Christian Llupo <christianllupo@protonmail.com>
2021-10-04 15:54:51 +0300

    Fixed wearnings of the function system

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,8 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended"
   ],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off"
+  }
 }
