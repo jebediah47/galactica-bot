d8fdd0dd2c76638857b4e44dd05b520901f33767
Christian Llupo <christianllupo@protonmail.com>
2021-10-04 15:48:10 +0300

    Rolled back to old function system
    Paler's function system was broken af

---
+++ src/functions/commaFormatter.ts
@@ -0,0 +1,5 @@
+export function commaFormatter(x) {
+  const parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+}
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,10 +1,4 @@
-export function commaFormatter(x) {
-  const parts = x.toString().split(".");
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  return parts.join(".");
-}
-
-export function capitalize(s) {
-  if (typeof s !== "string") return "";
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
+import { commaFormatter } from "./commaFormatter";
+import { capitalize } from "./capitalize";
+export { commaFormatter };
+export { capitalize };
