822b39b46c667711d03083fb5a4ad5810c96eb43
Christian Llupo <christianllupo@protonmail.com>
2021-10-11 22:03:34 +0300

    Added member leave modlog

---
+++ src/events/guildMemberRemove.ts
@@ -0,0 +1,17 @@
+import { Event } from "../interfaces";
+
+export const event: Event = {
+  name: "guildMemberRemove",
+  run: async (client, member) => {
+    try {
+      const channel = member.guild.channels.cache.find(
+        (c) => c.name == client.config.MODLOGS_CHANNEL_NAME
+      );
+      channel.send(`[LOGS] A new member has left ${member.user.tag}`);
+    } catch (err) {
+      console.log(
+        "A problem has occurred please check you config.json file on the MODLOGS_CHANNEL_NAME parameter and see if you have typed a wrong channel name"
+      );
+    }
+  },
+};
