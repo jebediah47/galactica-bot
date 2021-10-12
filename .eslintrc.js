9a1345f3d9a9b2bf577f407050bcc02584066294
Christian Llupo <christianllupo@protonmail.com>
2021-10-12 21:57:36 +0300

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   ],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off"
   }
 }
