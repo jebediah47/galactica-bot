f0d4d3445b0195c71b43f2ff18f2236b099f421a
Christian Llupo <jebediah47@protonmail.com>
2021-12-03 20:18:33 +0200

    Updated ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,10 @@
+import { stdout } from "process";
 import { Event } from "../interfaces";
 
 export const event: Event = {
   name: "ready",
   run: (client) => {
-    console.log(`We have logged in as ${client.user.tag}`);
+    stdout.write(`We have logged in as ${client.user.tag}`);
     client.user.setActivity(`${client.config.BOT_PRESENCE}`, {
       type: "COMPETING",
     });
