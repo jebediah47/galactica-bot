e13c86023b2ec0805862ca8ae84a03bcd9ddff45
Christian Llupo <jebediah47@protonmail.com>
2022-03-27 13:01:53 +0300

    Fixed Windows IDE error with prettier on .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
       ],
       rules: {
         "@typescript-eslint/explicit-module-boundary-types": "off",
+        "prettier/prettier": ["error", { endOfLine: "auto" }],
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/ban-ts-comment": "off",
         "no-console": 1,
