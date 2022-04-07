18b252f7c6c818ee9670d3002780fe9aaec32680
Christian Llupo <jebediah47@protonmail.com>
2022-04-07 14:56:27 +0300

    Added error handling in dictionary.ts

--- src/commands/misc/dictionary.ts
+++ src/commands/misc/dictionary.ts
@@ -6,7 +6,7 @@ export const command: Command = {
   name: "dictionary",
   aliases: ["urban-dictionary"],
   run: async (client, message, args) => {
-    let query: any = args.join(" ");
+    let query: string = args.join(" ");
     if (!query) {
       const embed = new MessageEmbed()
         .setColor("RANDOM")
@@ -16,26 +16,33 @@ export const command: Command = {
       return message.reply({ embeds: [embed] });
     }
 
-    query = encodeURIComponent(query);
-    const {
-      data: { list },
-    } = await axios.get(
-      `https://api.urbandictionary.com/v0/define?term=${query}`
-    );
-    const [answer] = list;
+    try {
+      query = encodeURIComponent(query);
+      const {
+        data: { list },
+      } = await axios.get(
+        `https://api.urbandictionary.com/v0/define?term=${query}`
+      );
+      const [answer] = list;
 
-    const embed2 = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle(answer.word)
-      .setURL(answer.permalink)
-      .addField("Definition:", trim(answer.definition))
-      .addField("Example:", trim(answer.example))
-      .addField("Ratings:", `${answer.thumbs_up} 👍   ${answer.thumbs_down} 👎`)
-      .setTimestamp();
-    message.channel.send({ embeds: [embed2] });
+      const embed2 = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(answer.word)
+        .setURL(answer.permalink)
+        .addField("Definition:", trim(answer.definition))
+        .addField("Example:", trim(answer.example))
+        .addField(
+          "Ratings:",
+          `${answer.thumbs_up} 👍   ${answer.thumbs_down} 👎`
+        )
+        .setTimestamp();
+      message.channel.send({ embeds: [embed2] });
+    } catch (err) {
+      message.channel.send("An error occurred. " + err);
+    }
   },
 };
 
-function trim(input) {
+function trim(input: string) {
   return input.length > 1024 ? `${input.slice(0, 1020)} ... ` : input;
 }
