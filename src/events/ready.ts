3b6297264ce6d284afe70d3230ada152c5ad29c7
Christian Llupo <jebediah47@protonmail.com>
2022-04-07 15:07:49 +0300

    Updated ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,8 +3,10 @@ import { Event } from "../interfaces";
 export const event: Event = {
   name: "ready",
   run: (client) => {
+    //@ts-ignore
     process.stdout.write(`We have logged in as ${client.user.tag} \n`);
     // eslint-disable-next-line no-console
+    //@ts-ignore
     client.user.setActivity(`${client.config.BOT_PRESENCE}`, {
       //@ts-ignore
       type: `${client.config.BOT_PRESENCE_TYPE}`,
