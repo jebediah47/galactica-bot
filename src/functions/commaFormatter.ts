3312f416de9870d801423125f0b1666efdabf119
Christian Llupo <jebediah47@protonmail.com>
2022-04-07 15:08:56 +0300

    Updated commaFormatted.ts

--- src/functions/commaFormatter.ts
+++ src/functions/commaFormatter.ts
@@ -1,4 +1,4 @@
-export function commaFormatter(x) {
+export function commaFormatter(x: string) {
   const parts = x.toString().split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   return parts.join(".");
