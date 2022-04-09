de8e69ecbd0bb182573ed1789e20fb1bcbf310c3
Christian Llupo <jebediah47@protonmail.com>
2022-04-09 17:31:29 +0300

    Fixed types in ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,12 +3,11 @@ import { Event } from "../interfaces";
 export const event: Event = {
   name: "ready",
   run: (client) => {
-    //@ts-ignore
-    process.stdout.write(`We have logged in as ${client.user.tag} \n`);
-    //@ts-ignore
-    client.user.setActivity(`${client.config.BOT_PRESENCE}`, {
-      //@ts-ignore
-      type: `${client.config.BOT_PRESENCE_TYPE}`,
-    });
+    if (client.user != null) {
+      process.stdout.write(`We have logged in as ${client.user.tag} \n`);
+      client.user.setActivity(`${client.config.BOT_PRESENCE}`, {
+        type: client.config.BOT_PRESENCE_TYPE,
+      });
+    }
   },
 };
