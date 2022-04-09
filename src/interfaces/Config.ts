275aafa3a7cb2c6a109e1113f9f76952de75fa8e
Christian Llupo <jebediah47@protonmail.com>
2022-04-09 17:31:06 +0300

    Fixed types in Config.ts

--- src/interfaces/Config.ts
+++ src/interfaces/Config.ts
@@ -1,7 +1,10 @@
+import { ActivityTypes } from "discord.js/typings/enums";
+import { ExcludeEnum } from "discord.js";
+
 export interface Config {
   TOKEN: string;
   PREFIX: string;
   BOT_PRESENCE: string;
-  BOT_PRESENCE_TYPE: string;
+  BOT_PRESENCE_TYPE: ExcludeEnum<typeof ActivityTypes, "CUSTOM">;
   MODLOGS_CHANNEL_NAME: string;
 }
