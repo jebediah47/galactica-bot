517575658024ff61b81dc59e3967d43dbd6d4081
Christian Llupo <jebediah47@protonmail.com>
2022-04-10 17:43:29 +0300

    Added disconnect.ts music command

---
+++ src/commands/music/disconnect.ts
@@ -0,0 +1,9 @@
+import { Command } from "../../interfaces";
+
+export const command: Command = {
+  name: "disconnect",
+  aliases: ["leave"],
+  run: (client, message) => {
+    client.distube?.voices.leave(message);
+  },
+};
