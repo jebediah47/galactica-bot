3f1f18780ead16154995aa14642b4b3843cc7866
Christian Llupo <jebediah47@protonmail.com>
2022-04-10 17:43:07 +0300

    Updated playSong.ts event

--- src/events/playSong.ts
+++ src/events/playSong.ts
@@ -1,11 +1,45 @@
+import { MessageEmbed } from "discord.js";
 import { Queue, Song } from "distube";
 import { Event } from "../interfaces";
 
 export const event: Event = {
   name: "playSong",
   run: (client, queue: Queue, song: Song) => {
-    queue.textChannel?.send(
-      `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}`
-    );
+    const embed = new MessageEmbed()
+      .setTitle("Playing a song...")
+      .setColor("RANDOM")
+      .setDescription(`Song: [\`${song.name}\`](${song.url})`)
+      .addField("💡 Requested by:", `>>> ${song.user}`, true)
+      .addField(
+        "⏱ Duration:",
+        `>>> \`${queue.formattedCurrentTime} / ${song.formattedDuration}\``,
+        true
+      )
+      .addField(
+        "🌀 Queue:",
+        `>>> \`${queue.songs.length} song(s) - ${queue.formattedDuration}\``,
+        true
+      )
+      .addField("🔊 Volume:", `>>> \`${queue.volume} %\``, true)
+      .addField(
+        "♾ Loop:",
+        `>>> ${
+          queue.repeatMode
+            ? queue.repeatMode === 2
+              ? "✅ Queue"
+              : "✅ Song"
+            : "❌"
+        }`,
+        true
+      )
+      .addField(
+        "❔ Download Song:",
+        `>>> [\`Click here\`](${song.streamURL})`,
+        true
+      )
+      .setTimestamp()
+      //@ts-ignore
+      .setImage(song.thumbnail);
+    queue.textChannel?.send({ embeds: [embed] });
   },
 };
