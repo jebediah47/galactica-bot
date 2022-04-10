e4ca5efdc90204f12949df0bcfc1a9ee932e5c69
Christian Llupo <jebediah47@protonmail.com>
2022-04-10 22:05:04 +0300

    Updated volume.ts

--- src/commands/music/volume.ts
+++ src/commands/music/volume.ts
@@ -13,13 +13,25 @@ export const command: Command = {
       .setColor("RANDOM")
       .setTitle("Notice!")
       .setDescription(
-        "You **must** enter a valid number from the ranmmge 1 - 100"
+        "You **must** enter a valid number from the range 1 - 100"
       )
       .setTimestamp();
     const volume = parseInt(args[0]);
     if (isNaN(volume) || !args[0])
       return message.channel.send({ embeds: [errEmbed] });
+
+    if (volume > 100) {
+      const errEmbed2 = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("Volume cannot be over 100%")
+        .setTimestamp();
+      return message.channel.send({ embeds: [errEmbed2] });
+    }
     queue.setVolume(volume);
-    message.channel.send(`Volume set to \`${volume}\``);
+    const volume_embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle(`🔊Set volume to \`${volume}\`%`)
+      .setTimestamp();
+    message.channel.send({ embeds: [volume_embed] });
   },
 };
