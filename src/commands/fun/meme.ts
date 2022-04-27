e5c0ae976618b53e04b1d6daa5cf8fea289c8c00
Christian Llupo <jebediah47@protonmail.com>
2022-04-27 12:00:22 +0300

    Added error handling to meme.ts

--- src/commands/fun/meme.ts
+++ src/commands/fun/meme.ts
@@ -10,25 +10,33 @@ export const command: Command = {
     const data = ["dankmemes", "meme", "memes", "terriblefacebookmemes"];
     const subreddit = data[Math.floor(Math.random() * data.length)];
 
-    got(`https://www.reddit.com/r/${subreddit}/random/.json`).then(
-      (response) => {
-        const content = JSON.parse(response.body);
-        const permalink = content[0].data.children[0].data.permalink;
-        const memeUrl = `https://reddit.com${permalink}`;
-        const memeImage = content[0].data.children[0].data.url;
-        const memeTitle = content[0].data.children[0].data.title;
-        const memeUpvotes = content[0].data.children[0].data.ups;
-        const memeDownvotes = content[0].data.children[0].data.downs;
-        const memeNumComments = content[0].data.children[0].data.num_comments;
-        embed.setTitle(`${memeTitle}`);
-        embed.setURL(`${memeUrl}`);
-        embed.setImage(memeImage);
-        embed.setColor("RANDOM");
-        embed.setFooter({
-          text: `From r/${subreddit} 👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`,
-        });
-        message.channel.send({ embeds: [embed] });
-      }
-    );
+    try {
+      got(`https://www.reddit.com/r/${subreddit}/random/.json`).then(
+        (response) => {
+          const content = JSON.parse(response.body);
+          const permalink = content[0].data.children[0].data.permalink;
+          const memeUrl = `https://reddit.com${permalink}`;
+          const memeImage = content[0].data.children[0].data.url;
+          const memeTitle = content[0].data.children[0].data.title;
+          const memeUpvotes = content[0].data.children[0].data.ups;
+          const memeDownvotes = content[0].data.children[0].data.downs;
+          const memeNumComments = content[0].data.children[0].data.num_comments;
+          embed.setTitle(`${memeTitle}`);
+          embed.setURL(`${memeUrl}`);
+          embed.setImage(memeImage);
+          embed.setColor("RANDOM");
+          embed.setFooter({
+            text: `From r/${subreddit} 👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`,
+          });
+          message.channel.send({ embeds: [embed] });
+        }
+      );
+    } catch (err) {
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setDescription(`${err}`)
+        .setTimestamp();
+      return message.reply({ embeds: [embed] });
+    }
   },
 };
