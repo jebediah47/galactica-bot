aeaa5f02840ce021b183fec731bd8d3b7333f61e
Christian Llupo <jebediah47@protonmail.com>
2022-04-29 12:13:00 +0300

    Fixed tsignore error in addSong.ts

--- src/events/addSong.ts
+++ src/events/addSong.ts
@@ -5,20 +5,21 @@ import { Event } from "../interfaces";
 export const event: Event = {
   name: "addSong",
   run: (client, queue: Queue, song: Song) => {
-    const embed = new MessageEmbed()
-      .setTitle("Added a son to queue...")
-      .setColor("RANDOM")
-      .setDescription(`Song: [\`${song.name}\`](${song.url})`)
-      .addField("💡 Requested by:", `>>> ${song.user}`, true)
-      .addField("⏱ Duration:", `>>> \`${song.formattedDuration}\``, true)
-      .addField(
-        "🌀 Queue:",
-        `>>> \`${queue.songs.length} song(s) - ${queue.formattedDuration}\``,
-        true
-      )
-      .setTimestamp()
-      //@ts-ignore
-      .setImage(song.thumbnail);
-    queue.textChannel?.send({ embeds: [embed] });
+    if (typeof song.thumbnail === "string") {
+      const embed = new MessageEmbed()
+        .setTitle("Added a son to queue...")
+        .setColor("RANDOM")
+        .setDescription(`Song: [\`${song.name}\`](${song.url})`)
+        .addField("💡 Requested by:", `>>> ${song.user}`, true)
+        .addField("⏱ Duration:", `>>> \`${song.formattedDuration}\``, true)
+        .addField(
+          "🌀 Queue:",
+          `>>> \`${queue.songs.length} song(s) - ${queue.formattedDuration}\``,
+          true
+        )
+        .setTimestamp()
+        .setImage(song.thumbnail);
+      queue.textChannel?.send({ embeds: [embed] });
+    }
   },
 };
