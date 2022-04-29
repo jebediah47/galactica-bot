09e7545b06bb02e1026e478c63f99251a9fa05f5
Christian Llupo <jebediah47@protonmail.com>
2022-04-29 12:13:12 +0300

    Fixed tsignore error in playSong.ts

--- src/events/playSong.ts
+++ src/events/playSong.ts
@@ -5,37 +5,39 @@ import { Event } from "../interfaces";
 export const event: Event = {
   name: "playSong",
   run: (client, queue: Queue, song: Song) => {
-    const embed = new MessageEmbed()
-      .setTitle("Playing a song...")
-      .setColor("RANDOM")
-      .setDescription(`Song: [\`${song.name}\`](${song.url})`)
-      .addField("💡 Requested by:", `>>> ${song.user}`, true)
-      .addField("⏱ Duration:", `>>> \`${song.formattedDuration}\``, true)
-      .addField(
-        "🌀 Queue:",
-        `>>> \`${queue.songs.length} song(s) - ${queue.formattedDuration}\``,
-        true
-      )
-      .addField("🔊 Volume:", `>>> \`${queue.volume} %\``, true)
-      .addField(
-        "♾ Loop:",
-        `>>> ${
-          queue.repeatMode
-            ? queue.repeatMode === 2
-              ? "✅ Queue"
-              : "✅ Song"
-            : "❌"
-        }`,
-        true
-      )
-      .addField(
-        "❔ Download Song:",
-        `>>> [\`Click here\`](${song.streamURL})`,
-        true
-      )
-      .setTimestamp()
-      //@ts-ignore
-      .setImage(song.thumbnail);
-    queue.textChannel?.send({ embeds: [embed] });
+    if (typeof song.thumbnail === "string") {
+      const embed = new MessageEmbed()
+        .setTitle("Playing a song...")
+        .setColor("RANDOM")
+        .setDescription(`Song: [\`${song.name}\`](${song.url})`)
+        .addField("💡 Requested by:", `>>> ${song.user}`, true)
+        .addField("⏱ Duration:", `>>> \`${song.formattedDuration}\``, true)
+        .addField(
+          "🌀 Queue:",
+          `>>> \`${queue.songs.length} song(s) - ${queue.formattedDuration}\``,
+          true
+        )
+        .addField("🔊 Volume:", `>>> \`${queue.volume} %\``, true)
+        .addField(
+          "♾ Loop:",
+          `>>> ${
+            queue.repeatMode
+              ? queue.repeatMode === 2
+                ? "✅ Queue"
+                : "✅ Song"
+              : "❌"
+          }`,
+          true
+        )
+        .addField(
+          "❔ Download Song:",
+          `>>> [\`Click here\`](${song.streamURL})`,
+          true
+        )
+        .setTimestamp()
+        .setImage(song.thumbnail);
+
+      queue.textChannel?.send({ embeds: [embed] });
+    }
   },
 };
