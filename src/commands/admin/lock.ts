73205fe43213848a920393bc9ff216ef56ed296a
Christian Llupo <jebediah47@protonmail.com>
2022-06-01 12:38:06 +0300

    Updated lock.ts dir

---
+++ src/commands/admin/lock.ts
@@ -0,0 +1,30 @@
+import { Command } from "../../interfaces";
+
+export const command: Command = {
+  name: "lock",
+  description: "Locks a locked channel.",
+  options: [
+    {
+      name: "channel",
+      description: "Channel name",
+      type: "CHANNEL",
+      required: false,
+    },
+  ],
+  run: (client, interaction, args) => {
+    if (!interaction.member.permissions.has("MANAGE_CHANNELS")) {
+      return interaction.reply("You are not permitted to use this command!");
+    }
+    const everyone: any = interaction.guild?.roles.cache.find(
+      (r) => r.name === "@everyone"
+    );
+    let channel: any = args.getChannel("channel");
+    if (!channel) channel = interaction.channel;
+    if (channel.type === "GUILD_TEXT") {
+      channel.permissionOverwrites.edit(everyone, {
+        SEND_MESSAGES: false,
+      });
+      interaction.reply("🔒 The channel has been locked");
+    }
+  },
+};
