9890ca878ef75a3cdb9a395879907b988331f51e
Christian Llupo <jebediah47@protonmail.com>
2022-06-01 12:42:12 +0300

    Updated repeat.ts for slashcommand support

--- src/commands/music/repeat.ts
+++ src/commands/music/repeat.ts
@@ -3,19 +3,27 @@ import { MessageEmbed } from "discord.js";
 
 export const command: Command = {
   name: "repeat",
-  aliases: ["rp", "loop"],
-  run: (client, message, args) => {
-    const queue = client.distube?.getQueue(message);
+  description: "Disables or enables repeat mode.",
+  options: [
+    {
+      name: "mode",
+      description: "Sets the repeat mode",
+      type: "STRING",
+      required: true,
+    },
+  ],
+  run: (client, interaction, args) => {
+    const queue = client.distube?.getQueue(interaction);
     if (!queue) {
       const nothingPlaying = new MessageEmbed()
         .setColor("RANDOM")
         .setTitle("❌ Error!")
         .setDescription("There is nothing playing!")
         .setTimestamp();
-      return message.channel.send({ embeds: [nothingPlaying] });
+      return interaction.reply({ embeds: [nothingPlaying] });
     }
     let mode;
-    switch (args[0]) {
+    switch (args.getString("mode")) {
       case "off":
         mode = 0;
         break;
@@ -26,25 +34,7 @@ export const command: Command = {
         mode = 2;
         break;
     }
-    const errEmbed = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle("No repeat option set!")
-      .setDescription(
-        `To enable repeat you need to mention one of the following modes \n` +
-          // eslint-disable-next-line prettier/prettier
-           "\`0\` **off** \n" +
-          "This is self explanatory, it switches the repeat mode off. \n" +
-          // eslint-disable-next-line prettier/prettier
-           "\`1\` **song** \n" +
-          "This mode will enable the repeat mode only in the currently playing song. \n" +
-          // eslint-disable-next-line prettier/prettier
-           "\`2\` **queue** \n" +
-          "This one will enable repeat mode throughout the queue."
-      )
-      .setTimestamp();
-    if (!args[0]) {
-      return message.channel.send({ embeds: [errEmbed] });
-    }
+    if (!args.getString("mode")) return;
     try {
       mode = queue.setRepeatMode(mode);
       mode = mode ? (mode === 2 ? "Repeat queue" : "Repeat song") : "Off";
@@ -52,9 +42,9 @@ export const command: Command = {
         .setColor("RANDOM")
         .setTitle(`Set repeat mode to \`${mode}\``)
         .setTimestamp();
-      message.channel.send({ embeds: [mode_embed] });
+      interaction.reply({ embeds: [mode_embed] });
     } catch (err) {
-      return message.channel.send(`${err}`);
+      return interaction.reply(`${err}`);
     }
   },
 };
