af80d04a8d3e0e3b6c8213c87dc3cb08464f5221
Christian Llupo <jebediah47@protonmail.com>
2022-06-06 21:12:40 +0300

    Updated disconnect.ts

--- src/commands/music/disconnect.ts
+++ src/commands/music/disconnect.ts
@@ -4,7 +4,6 @@ export const command: Command = {
   name: "disconnect",
   description: "Disconnects the bot from current voice channel.",
   run: (client, interaction) => {
-    // @ts-ignore
     client.distube?.voices.leave(interaction);
     interaction.reply("👋");
   },
