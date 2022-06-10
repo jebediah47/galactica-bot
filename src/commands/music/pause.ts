7cd57bbd240ac194d4a9a8b9211e6e2dd0e7df23
Christian Llupo <jebediah47@protonmail.com>
2022-06-10 22:53:58 +0300

    Added pause.ts (rename)

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,33 @@
+import { MessageEmbed } from "discord.js";
+import { Command } from "../../interfaces";
+
+export const command: Command = {
+  name: "pause",
+  description: "Pauses the song currently playing",
+  run: (client, interaction) => {
+    const queue = client.distube?.getQueue(interaction);
+    if (!queue) {
+      const noQueue = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("❌ Error!")
+        .setDescription("There is nothing in the queue right now!")
+        .setTimestamp();
+      return interaction.reply({ embeds: [noQueue] });
+    } else if (queue.paused) {
+      queue.resume();
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setDescription(
+          "The song was already paused but I resumed it for you 😃"
+        )
+        .setTimestamp();
+      return interaction.reply({ embeds: [embed] });
+    }
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription("The queue has been stopped!")
+      .setTimestamp();
+    interaction.reply({ embeds: [embed] });
+    client.distube?.pause(interaction);
+  },
+};
