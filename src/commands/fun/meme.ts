89e61a8e2794585c9eff92f36d31eea529323c2b
Christian Llupo <jebediah47@protonmail.com>
2022-06-10 15:28:20 +0300

    Added meme.ts

---
+++ src/commands/fun/meme.ts
@@ -0,0 +1,21 @@
+import { Command } from "../../interfaces";
+import { MessageEmbed } from "discord.js";
+import randomPuppy from "random-puppy";
+
+export const command: Command = {
+  name: "meme",
+  description: "Sends you an EPICO meme!",
+  run: async (client, interaction) => {
+    const subReddits = ["meme", "memes", "raimimemes", "terriblefacebookmemes"];
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+    const image = await randomPuppy(random);
+    const meme = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle(`Here is your meme from r/${random}`)
+      .setURL(`https://reddit.com/r/${random}`)
+      .setImage(image)
+      .setTimestamp();
+    interaction.reply({ embeds: [meme] });
+  },
+};
