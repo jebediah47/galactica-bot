9943bb00a212be8f4970f99f350581f0d786959e
Christian Llupo <jebediah47@protonmail.com>
2022-06-10 12:11:31 +0300

    Added rps.ts

---
+++ src/commands/fun/rps.ts
@@ -0,0 +1,13 @@
+import RPS from "discord-rock-paper-scissor";
+import { Command } from "../../interfaces";
+
+export const command: Command = {
+  name: "rps",
+  description:
+    "Starts a rock-paper-scissors game with the bot as the opponent.",
+  run: async (client, interaction) => {
+    interaction.reply(`RPS game, ${interaction.user.username} vs BOT`);
+    const rps = new RPS();
+    rps.solo(interaction, client);
+  },
+};
