03f94e30cdbd9d2bc1f4222b6dc43b46ffc95f84
Christian Llupo <jebediah47@protonmail.com>
2022-06-10 22:53:28 +0300

    Fixed disconnect.ts bug

--- src/commands/music/disconnect.ts
+++ src/commands/music/disconnect.ts
@@ -1,10 +1,20 @@
 import { Command } from "../../interfaces";
+import { MessageEmbed } from "discord.js";
 
 export const command: Command = {
   name: "disconnect",
   description: "Disconnects the bot from current voice channel.",
   run: (client, interaction) => {
-    client.distube?.voices.leave(interaction);
-    interaction.reply("👋");
+    if (client.distube?.voices.has(interaction)) {
+      client.distube?.voices.leave(interaction);
+      interaction.reply("👋");
+    } else {
+      const noVoiceChannel = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("❌ Error!")
+        .setDescription("The bot is not connected to any voice-channel!")
+        .setTimestamp();
+      return interaction.reply({ embeds: [noVoiceChannel] });
+    }
   },
 };
