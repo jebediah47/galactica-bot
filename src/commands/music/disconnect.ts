c9c7305a1c689dc874cfb3aceb1ab34d9e3556eb
Christian Llupo <jebediah47@protonmail.com>
2022-06-11 13:21:32 +0300

    Update disconnect.ts

--- src/commands/music/disconnect.ts
+++ src/commands/music/disconnect.ts
@@ -4,10 +4,10 @@ import { MessageEmbed } from "discord.js";
 export const command: Command = {
   name: "disconnect",
   description: "Disconnects the bot from current voice channel.",
-  run: (client, interaction) => {
+  run: async (client, interaction) => {
     if (client.distube?.voices.has(interaction)) {
       client.distube?.voices.leave(interaction);
-      interaction.reply("👋");
+      await interaction.reply("👋");
     } else {
       const noVoiceChannel = new MessageEmbed()
         .setColor("RANDOM")
