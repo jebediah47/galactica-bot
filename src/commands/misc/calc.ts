0bbef8006bdc9aefff80a9dfd96417b57e6ddad1
Christian Llupo <jebediah47@protonmail.com>
2022-06-11 13:29:27 +0300

    Create calc.ts

---
+++ src/commands/misc/calc.ts
@@ -0,0 +1,11 @@
+import { Command } from "../../interfaces";
+import { calc } from "../../functions";
+
+export const command: Command = {
+  name: "calc",
+  description: "Evaluates math problems.",
+  run: async (client, interaction) => {
+    await interaction.deferReply();
+    await calc(interaction);
+  },
+};
