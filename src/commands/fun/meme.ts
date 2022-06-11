1f7a1caa59e5aece0287b32def40552a03e89970
Christian Llupo <jebediah47@protonmail.com>
2022-06-11 16:53:32 +0300

    Update meme.ts

--- src/commands/fun/meme.ts
+++ src/commands/fun/meme.ts
@@ -1,21 +1,42 @@
 import { Command } from "../../interfaces";
 import { MessageEmbed } from "discord.js";
-import randomPuppy from "random-puppy";
+import got from "got";
 
 export const command: Command = {
   name: "meme",
   description: "Sends you an EPICO meme!",
   run: async (client, interaction) => {
-    const subReddits = ["meme", "memes", "raimimemes", "terriblefacebookmemes"];
+    const subReddits = [
+      "meme",
+      "memes",
+      "terriblefacebookmemes",
+      "dankmemes",
+      "PewdiepieSubmissions",
+      "MemeEconomy",
+    ];
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+    await got(`https://www.reddit.com/r/${random}/random/.json`).then(
+      async (response) => {
+        const [list] = JSON.parse(response.body);
+        const [post] = list.data.children;
 
-    const image = await randomPuppy(random);
-    const meme = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle(`Here is your meme from r/${random}`)
-      .setURL(`https://reddit.com/r/${random}`)
-      .setImage(image)
-      .setTimestamp();
-    await interaction.reply({ embeds: [meme] });
+        const permalink = post.data.permalink;
+        const memeUrl = `https://reddit.com${permalink}`;
+        const memeImage = post.data.url;
+        const memeTitle = post.data.title;
+        const memeUpvotes = post.data.ups;
+        const memeNumComments = post.data.num_comments;
+        const meme = new MessageEmbed()
+          .setTitle(`${memeTitle}`)
+          .setURL(`${memeUrl}`)
+          .setColor("RANDOM")
+          .setImage(memeImage)
+          .setFooter({
+            text: `👍 ${memeUpvotes} 💬 ${memeNumComments}`,
+          });
+
+        await interaction.reply({ embeds: [meme] });
+      }
+    );
   },
 };
