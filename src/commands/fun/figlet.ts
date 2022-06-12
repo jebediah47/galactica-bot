3e253be8fe4fd7231188969352d76d16e3565532
Christian Llupo <jebediah47@protonmail.com>
2022-06-12 13:44:06 +0300

    Create figlet.ts

---
+++ src/commands/fun/figlet.ts
@@ -0,0 +1,26 @@
+import { Command } from "../../interfaces";
+import util from "node:util";
+import figlet from "figlet";
+
+export const command: Command = {
+  name: "figlet",
+  description: "Creates figlet (ascii) art from input.",
+  options: [
+    {
+      name: "input",
+      description: "Input to be converted to ascii",
+      type: "STRING",
+      required: true,
+    },
+  ],
+  run: async (client, interaction, args) => {
+    const fig = util.promisify(figlet);
+    const input = args.getString("input");
+    if (!input || input.length > 20) {
+      return interaction.reply({
+        content: "Input must be up to `20` characters!",
+      });
+    }
+    await interaction.reply({ content: `\`\`\`${await fig(input)}\`\`\`` });
+  },
+};
