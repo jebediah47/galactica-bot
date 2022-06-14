fc51e9f1cd8b220a516fbddf9885cf62ff7531cd
Christian Llupo <jebediah47@protonmail.com>
2022-06-14 23:06:03 +0300

    Create kanye.ts

---
+++ src/commands/misc/kanye.ts
@@ -0,0 +1,11 @@
+import { Command } from "../../interfaces";
+import axios from "axios";
+
+export const command: Command = {
+  name: "kanyewest",
+  description: "Sends a random quote said by Kanye West",
+  run: async (client, interaction) => {
+    const { data } = await axios.get("https://api.kanye.rest/");
+    await interaction.reply({ content: data.quote });
+  },
+};
