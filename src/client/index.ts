ff8c9bd4a1c4da2ae3cc1c806f0b342d031278fb
Christian Llupo <jebediah47@protonmail.com>
2022-06-18 09:10:54 +0300

    Updated client

--- src/client/index.ts
+++ src/client/index.ts
@@ -24,19 +24,19 @@ class ExtendedClient extends Client {
       intents: 32767,
     });
   }
-  public registerCommands({ commands }: RegisterCommandOptions) {
-    this.application?.commands.set(commands);
+  public async registerCommands({ commands }: RegisterCommandOptions) {
+    await this.application?.commands.set(commands);
   }
   public async init(): Promise<void> {
-    this.login(this.config.TOKEN);
+    this.login(this.config.TOKEN).then();
     const slashCommands: ApplicationCommandDataResolvable[] = [];
     const command_files = path.join(__dirname, "..", "commands");
-    (await readdir(command_files)).forEach(async (dir) => {
+    for (const dir of (await readdir(command_files))) {
       const commands = (await readdir(`${command_files}/${dir}`)).filter(
         (file) => file.endsWith(".ts") || file.endsWith(".js") // the second filter is for when the bot is built in JS
       );
       if (command_files.length <= 0) {
-        return stdout.write("[LOGS] Couldn't Find Commands!");
+        stdout.write("[LOGS] Couldn't Find Commands!");
       }
 
       for (const file of commands) {
@@ -44,25 +44,25 @@ class ExtendedClient extends Client {
         this.commands.set(command.name, command);
         slashCommands.push(command);
       }
-    });
+    }
     this.on("ready", () => {
       this.registerCommands({
         commands: slashCommands,
       });
     });
     const event_files = path.join(__dirname, "..", "events");
-    (await readdir(event_files)).forEach(async (file) => {
+    for (const file of (await readdir(event_files))) {
       const { event } = await import(`${event_files}/${file}`);
       this.events.set(event.name, event);
       this.on(event.name, event.run.bind(null, this));
       this.distube
         ?.on(event.name, event.run.bind(null, this))
         .setMaxListeners(2);
-    });
-    if (this.config.SERVER_OPTIONS.ENABLED === true) {
+    }
+    if (this.config.SERVER_OPTIONS.ENABLED) {
       galacticaServer(this.config.SERVER_OPTIONS.PORT);
     }
-    if (this.config.MUSIC_IS_ENABLED === true) {
+    if (this.config.MUSIC_IS_ENABLED) {
       this.distube = new DisTube(this, {
         searchSongs: 0,
         emitNewSongOnly: true,
