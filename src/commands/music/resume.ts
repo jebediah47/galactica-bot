21748dd394833594c55e401921d55f9b0c9a4b69
jebediah47 <jebediah47@protonmail.com>
2022-07-11 13:58:06 +0300

    Updated resume.ts

--- src/commands/music/resume.ts
+++ src/commands/music/resume.ts
@@ -5,29 +5,35 @@ export const command: Command = {
   name: "resume",
   description: "Resumes the queue.",
   run: async (client, interaction) => {
-    const queue = client.distube?.getQueue(interaction);
-    if (!queue) {
-      const noQueue = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription("There is nothing in queue!")
-        .setTimestamp();
-      return interaction.reply({ embeds: [noQueue] });
-    }
-    try {
-      queue.resume();
-      const embed = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("The song has been resumed!")
-        .setTimestamp();
-      await interaction.reply({ embeds: [embed] });
-    } catch (err) {
-      const errEmbed = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription(`${err}`)
-        .setTimestamp();
-      return interaction.reply({ embeds: [errEmbed] });
+    if (client.config.MUSIC_IS_ENABLED) {
+      const queue = client.distube?.getQueue(interaction);
+      if (!queue) {
+        const noQueue = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription("There is nothing in queue!")
+          .setTimestamp();
+        return interaction.reply({ embeds: [noQueue] });
+      }
+      try {
+        queue.resume();
+        const embed = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("The song has been resumed!")
+          .setTimestamp();
+        await interaction.reply({ embeds: [embed] });
+      } catch (err) {
+        const errEmbed = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription(`${err}`)
+          .setTimestamp();
+        return interaction.reply({ embeds: [errEmbed] });
+      }
+    } else {
+      return interaction.reply({
+        content: "Music commands have been disabled by the owner.",
+      });
     }
   },
 };
