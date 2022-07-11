2ead21257fcc80482a5f3e00aee8eba78ab9d1aa
jebediah47 <jebediah47@protonmail.com>
2022-07-11 13:57:48 +0300

    Updated play.ts

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -13,25 +13,31 @@ export const command: Command = {
     },
   ],
   run: async (client, interaction, args) => {
-    if (!interaction.member.voice.channel) {
-      const noVoiceChannel = new MessageEmbed()
+    if (client.config.MUSIC_IS_ENABLED) {
+      if (!interaction.member.voice.channel) {
+        const noVoiceChannel = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription("You must be in a voice channel to use this command!")
+          .setTimestamp();
+        return interaction.reply({ embeds: [noVoiceChannel] });
+      }
+      const music = args.getString("song");
+      if (!music) return;
+      const success = new MessageEmbed()
         .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription("You must be in a voice channel to use this command!")
-        .setTimestamp();
-      return interaction.reply({ embeds: [noVoiceChannel] });
-    }
-    const music = args.getString("song");
-    if (!music) return;
-    const success = new MessageEmbed()
-      .setColor("RANDOM")
-      .setDescription("Added a song to the queue!");
-    if (interaction.channel?.type === "GUILD_TEXT") {
-      client.distube?.play(interaction.member.voice.channel, music, {
-        member: interaction.member,
-        textChannel: interaction.channel,
+        .setDescription("Added a song to the queue!");
+      if (interaction.channel?.type === "GUILD_TEXT") {
+        client.distube?.play(interaction.member.voice.channel, music, {
+          member: interaction.member,
+          textChannel: interaction.channel,
+        });
+        await interaction.reply({ embeds: [success] });
+      }
+    } else {
+      return interaction.reply({
+        content: "Music commands have been disabled by the owner.",
       });
-      await interaction.reply({ embeds: [success] });
     }
   },
 };
