cf3e6ad6e8c3c94068c9b5233970f70749728859
jebediah47 <jebediah47@protonmail.com>
2022-07-11 13:58:25 +0300

    Updated volume.ts

--- src/commands/music/volume.ts
+++ src/commands/music/volume.ts
@@ -14,37 +14,43 @@ export const command: Command = {
     },
   ],
   run: async (client, interaction, args) => {
-    const queue = client.distube?.getQueue(interaction);
-    if (!queue) {
-      const noQueue = new MessageEmbed()
+    if (client.config.MUSIC_IS_ENABLED) {
+      const queue = client.distube?.getQueue(interaction);
+      if (!queue) {
+        const noQueue = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription("There is nothing in queue!")
+          .setTimestamp();
+        return interaction.reply({ embeds: [noQueue] });
+      }
+      const errEmbed = new MessageEmbed()
         .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription("There is nothing in queue!")
+        .setTitle("Notice!")
+        .setDescription(
+          "You **must** enter a valid number from the range 1 - 100"
+        )
         .setTimestamp();
-      return interaction.reply({ embeds: [noQueue] });
-    }
-    const errEmbed = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle("Notice!")
-      .setDescription(
-        "You **must** enter a valid number from the range 1 - 100"
-      )
-      .setTimestamp();
-    const volume: any = args.getNumber("vol");
-    if (!isInteger(volume)) return interaction.reply({ embeds: [errEmbed] });
+      const volume: any = args.getNumber("vol");
+      if (!isInteger(volume)) return interaction.reply({ embeds: [errEmbed] });
 
-    if (volume > 100) {
-      const errEmbed2 = new MessageEmbed()
+      if (volume > 100) {
+        const errEmbed2 = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("Volume cannot be over 100%")
+          .setTimestamp();
+        return interaction.reply({ embeds: [errEmbed2] });
+      }
+      queue.setVolume(volume);
+      const volume_embed = new MessageEmbed()
         .setColor("RANDOM")
-        .setTitle("Volume cannot be over 100%")
+        .setTitle(`🔊Set volume to \`${volume}\`%`)
         .setTimestamp();
-      return interaction.reply({ embeds: [errEmbed2] });
+      await interaction.reply({ embeds: [volume_embed] });
+    } else {
+      return interaction.reply({
+        content: "Music commands have been disabled by the owner.",
+      });
     }
-    queue.setVolume(volume);
-    const volume_embed = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle(`🔊Set volume to \`${volume}\`%`)
-      .setTimestamp();
-    await interaction.reply({ embeds: [volume_embed] });
   },
 };
