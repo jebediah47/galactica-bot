4291b0547bb97c2b6d947e71e981dacb6da29298
jebediah47 <jebediah47@protonmail.com>
2022-07-11 18:20:05 +0300

    Updated types on ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,3 +1,5 @@
+import { ActivityTypes } from "discord.js/typings/enums";
+import { ExcludeEnum } from "discord.js";
 import { Event } from "../interfaces";
 
 export const event: Event = {
@@ -6,7 +8,9 @@ export const event: Event = {
     if (client.user != null) {
       process.stdout.write(`We have logged in as ${client.user.tag} \n`);
       client.user.setActivity(`${client.config.BOT_PRESENCE}`, {
-        type: client.config.BOT_PRESENCE_TYPE,
+        type: client.config.BOT_PRESENCE_TYPE as
+          | ExcludeEnum<typeof ActivityTypes, "CUSTOM">
+          | undefined,
       });
     }
   },
