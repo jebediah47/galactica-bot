59c246d7a35eda542739cf9878d3530435e3a599
jebediah47 <jebediah47@protonmail.com>
2022-07-11 13:57:41 +0300

    Updated pause.ts

--- src/commands/music/pause.ts
+++ src/commands/music/pause.ts
@@ -5,29 +5,35 @@ export const command: Command = {
   name: "pause",
   description: "Pauses the song currently playing",
   run: async (client, interaction) => {
-    const queue = client.distube?.getQueue(interaction);
-    if (!queue) {
-      const noQueue = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription("There is nothing in the queue right now!")
-        .setTimestamp();
-      return interaction.reply({ embeds: [noQueue] });
-    } else if (queue.paused) {
-      queue.resume();
+    if (client.config.MUSIC_IS_ENABLED) {
+      const queue = client.distube?.getQueue(interaction);
+      if (!queue) {
+        const noQueue = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription("There is nothing in the queue right now!")
+          .setTimestamp();
+        return interaction.reply({ embeds: [noQueue] });
+      } else if (queue.paused) {
+        queue.resume();
+        const embed = new MessageEmbed()
+          .setColor("RANDOM")
+          .setDescription(
+            "The song was already paused but I resumed it for you 😃"
+          )
+          .setTimestamp();
+        return interaction.reply({ embeds: [embed] });
+      }
       const embed = new MessageEmbed()
         .setColor("RANDOM")
-        .setDescription(
-          "The song was already paused but I resumed it for you 😃"
-        )
+        .setDescription("The queue has been stopped!")
         .setTimestamp();
-      return interaction.reply({ embeds: [embed] });
+      await interaction.reply({ embeds: [embed] });
+      client.distube?.pause(interaction);
+    } else {
+      return interaction.reply({
+        content: "Music commands have been disabled by the owner.",
+      });
     }
-    const embed = new MessageEmbed()
-      .setColor("RANDOM")
-      .setDescription("The queue has been stopped!")
-      .setTimestamp();
-    await interaction.reply({ embeds: [embed] });
-    client.distube?.pause(interaction);
   },
 };
