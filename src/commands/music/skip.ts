a4ee93bf2e93c2b1532d4dfe8869f82b9946612f
jebediah47 <jebediah47@protonmail.com>
2022-07-11 13:58:16 +0300

    Updated skip.ts

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -5,30 +5,36 @@ export const command: Command = {
   name: "skip",
   description: "Skips a one song in the queue.",
   run: async (client, interaction) => {
-    const queue = client.distube?.getQueue(interaction);
-    if (!queue) {
-      const noQueue = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription("There is nothing in queue!")
-        .setTimestamp();
-      return interaction.reply({ embeds: [noQueue] });
-    }
-    try {
-      await client.distube?.skip(interaction);
-      const embed = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("✅ Success!")
-        .setDescription("Skipped song!")
-        .setTimestamp();
-      await interaction.reply({ embeds: [embed] });
-    } catch (err) {
-      const errEmbed = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription(`${err}`)
-        .setTimestamp();
-      return interaction.reply({ embeds: [errEmbed] });
+    if (client.config.MUSIC_IS_ENABLED) {
+      const queue = client.distube?.getQueue(interaction);
+      if (!queue) {
+        const noQueue = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription("There is nothing in queue!")
+          .setTimestamp();
+        return interaction.reply({ embeds: [noQueue] });
+      }
+      try {
+        await client.distube?.skip(interaction);
+        const embed = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("✅ Success!")
+          .setDescription("Skipped song!")
+          .setTimestamp();
+        await interaction.reply({ embeds: [embed] });
+      } catch (err) {
+        const errEmbed = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription(`${err}`)
+          .setTimestamp();
+        return interaction.reply({ embeds: [errEmbed] });
+      }
+    } else {
+      return interaction.reply({
+        content: "Music commands have been disabled by the owner.",
+      });
     }
   },
 };
