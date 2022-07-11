8b75d89f29ddeb820cd01a44cbd2ce9dafe9386c
jebediah47 <jebediah47@protonmail.com>
2022-07-11 17:54:04 +0300

    Updates rps.ts

--- src/commands/fun/rps.ts
+++ src/commands/fun/rps.ts
@@ -6,8 +6,8 @@ export const command: Command = {
   description:
     "Starts a rock-paper-scissors game with the bot as the opponent.",
   run: async (client, interaction) => {
-    interaction.reply(`RPS game, ${interaction.user.username} vs BOT`);
+    await interaction.reply(`RPS game, ${interaction.user.username} vs BOT`);
     const rps = new RPS();
-    await rps.solo(interaction, client);
+    rps.solo(interaction, client);
   },
 };
