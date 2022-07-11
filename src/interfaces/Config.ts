9efe9c7d9600a53e4a4a7ac7349bfa33087624c1
jebediah47 <jebediah47@protonmail.com>
2022-07-11 18:19:48 +0300

    Fixed TypeError in client due to bad Config type

--- src/interfaces/Config.ts
+++ src/interfaces/Config.ts
@@ -1,11 +1,8 @@
-import { ActivityTypes } from "discord.js/typings/enums";
-import { ExcludeEnum } from "discord.js";
-
 export interface Config {
   TOKEN: string;
   BOT_PRESENCE: string;
   MUSIC_IS_ENABLED: boolean;
-  BOT_PRESENCE_TYPE: ExcludeEnum<typeof ActivityTypes, "CUSTOM"> | undefined;
+  BOT_PRESENCE_TYPE: string;
   MODLOGS_CHANNEL_NAME: string;
   SERVER_OPTIONS: {
     ENABLED: boolean;
