b22626601d9209bf6cc6d846f5e3057231b803d9
jebediah47 <jebediah47@protonmail.com>
2022-07-11 13:57:33 +0300

    Updated disconnect.ts

--- src/commands/music/disconnect.ts
+++ src/commands/music/disconnect.ts
@@ -5,16 +5,22 @@ export const command: Command = {
   name: "disconnect",
   description: "Disconnects the bot from current voice channel.",
   run: async (client, interaction) => {
-    if (client.distube?.voices.has(interaction)) {
-      client.distube?.voices.leave(interaction);
-      await interaction.reply({ content: "👋" });
+    if (client.config.MUSIC_IS_ENABLED) {
+      if (client.distube?.voices.has(interaction)) {
+        client.distube?.voices.leave(interaction);
+        await interaction.reply({ content: "👋" });
+      } else {
+        const noVoiceChannel = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle("❌ Error!")
+          .setDescription("The bot is not connected to any voice-channel!")
+          .setTimestamp();
+        return interaction.reply({ embeds: [noVoiceChannel] });
+      }
     } else {
-      const noVoiceChannel = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("❌ Error!")
-        .setDescription("The bot is not connected to any voice-channel!")
-        .setTimestamp();
-      return interaction.reply({ embeds: [noVoiceChannel] });
+      return interaction.reply({
+        content: "Music commands have been disabled by the owner.",
+      });
     }
   },
 };
