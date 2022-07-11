5252c133981a95739937e1b60f1a7a33ddc669e5
jebediah47 <jebediah47@protonmail.com>
2022-07-11 18:11:44 +0300

    Updated BOT_PRESENCE_TYPE on Config.ts

--- src/interfaces/Config.ts
+++ src/interfaces/Config.ts
@@ -1,8 +1,11 @@
+import { ActivityTypes } from "discord.js/typings/enums";
+import { ExcludeEnum } from "discord.js";
+
 export interface Config {
   TOKEN: string;
   BOT_PRESENCE: string;
   MUSIC_IS_ENABLED: boolean;
-  BOT_PRESENCE_TYPE: any;
+  BOT_PRESENCE_TYPE: ExcludeEnum<typeof ActivityTypes, "CUSTOM"> | undefined;
   MODLOGS_CHANNEL_NAME: string;
   SERVER_OPTIONS: {
     ENABLED: boolean;
