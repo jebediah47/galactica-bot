591ecb75fe84d210e8f6c750a0492372c1bfc78d
jebediah47 <jebediah47@protonmail.com>
2022-07-13 22:58:01 +0300

    Updated guildMemberAdd.ts

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,18 +1,28 @@
+import { GuildMember, TextChannel } from "discord.js";
 import { Event } from "../interfaces";
-import { TextChannel } from "discord.js";
 
 export const event: Event = {
   name: "guildMemberAdd",
-  run: async (client, member) => {
-    const channel: TextChannel = member.guild?.channels.cache.find(
-      (c: { name: string }) => c.name == client.config.MODLOGS_CHANNEL_NAME
-    ) as TextChannel;
+  run: async (client, member: GuildMember) => {
+    const db = await client.prisma.guildConfigs.findUnique({
+      where: {
+        guildID: `${member.guild.id}`,
+      },
+      select: {
+        modLogsIsEnabled: true,
+        modLogsChannelID: true,
+      },
+    });
+    if (db?.modLogsIsEnabled === true) {
+      const channel: TextChannel = member.guild.channels.cache.get(
+        db.modLogsChannelID as string
+      ) as TextChannel;
 
-    if (!channel) {
-      process.stderr.write(
-        "MODLOGS_CHANNEL_NAME cannot be found, please check your config.json \n"
-      );
+      if (!channel || member.user.tag === `${client.user?.tag}`) {
+        return;
+      }
+
+      await channel.send(`[LOGS] A new member has joined ${member.user.tag}`);
     }
-    await channel.send(`[LOGS] A new member has joined ${member.user.tag}`);
   },
 };
