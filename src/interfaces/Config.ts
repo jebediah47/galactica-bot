f9ece9756ad4d81de47b7194ff7ecb977b7ace4d
jebediah47 <jebediah47@protonmail.com>
2022-07-13 22:59:02 +0300

    Removed MUSIC_IS_ENABLED value in Config interface

--- src/interfaces/Config.ts
+++ src/interfaces/Config.ts
@@ -1,7 +1,6 @@
 export interface Config {
   TOKEN: string;
   BOT_PRESENCE: string;
-  MUSIC_IS_ENABLED: boolean;
   BOT_PRESENCE_TYPE: string;
   MODLOGS_CHANNEL_NAME: string;
   SERVER_OPTIONS: {
