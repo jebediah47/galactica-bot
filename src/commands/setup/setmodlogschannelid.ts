60012b2c271bada7ae67c8460e53c4e13628d0d1
jebediah47 <jebediah47@protonmail.com>
2022-07-13 22:57:04 +0300

    Created setmodlogschannelid.ts

---
+++ src/commands/setup/setmodlogschannelid.ts
@@ -0,0 +1,46 @@
+import { Command } from "../../interfaces";
+import { MessageEmbed } from "discord.js";
+
+export const command: Command = {
+  name: "setmodlogschannelid",
+  description: "Sets the mod-logs channel name",
+  options: [
+    {
+      name: "name",
+      description: "The mod-logs channel name, MUST BE UNIQUE",
+      type: "STRING",
+      required: true,
+    },
+  ],
+  run: async (client, interaction, args) => {
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(`Set modlogsChannelID to \`${args.getString("name")}\``)
+      .setTimestamp();
+
+    const db = await client.prisma.guildConfigs.findUniqueOrThrow({
+      where: {
+        guildID: `${interaction.guild?.id}`,
+      },
+      select: {
+        modLogsIsEnabled: true,
+      },
+    });
+    if (await db.modLogsIsEnabled) {
+      await client.prisma.guildConfigs.update({
+        where: {
+          guildID: `${interaction.guild?.id}`,
+        },
+        data: {
+          modLogsChannelID: args.getString("name"),
+        },
+      });
+      interaction.reply({ embeds: [embed] });
+    } else {
+      return interaction.reply({
+        content:
+          "modLogsIsEnabled database value is false, to enable run `/setmodlogs`",
+      });
+    }
+  },
+};
