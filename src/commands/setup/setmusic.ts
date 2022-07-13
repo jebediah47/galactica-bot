5c49f9ba5a03ec0589533d200edd8e028e1fdc38
jebediah47 <jebediah47@protonmail.com>
2022-07-13 22:57:15 +0300

    Created setmusic.ts

---
+++ src/commands/setup/setmusic.ts
@@ -0,0 +1,49 @@
+import { Command } from "../../interfaces";
+import { MessageEmbed } from "discord.js";
+
+export const command: Command = {
+  name: "setmusic",
+  description: "Enables or disables music commands.",
+  options: [
+    {
+      name: "mode",
+      description: "Sets the mode",
+      type: "STRING",
+      required: true,
+      choices: [
+        {
+          name: "true",
+          value: "true",
+        },
+        {
+          name: "false",
+          value: "false",
+        },
+      ],
+    },
+  ],
+  run: async (client, interaction, args) => {
+    let bool = false;
+    switch (args.get("mode")?.value) {
+      case "true":
+        bool = true;
+        break;
+      case "false":
+        bool = false;
+        break;
+    }
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(`Set musicIsEnabled to \`${bool}\``)
+      .setTimestamp();
+    await client.prisma.guildConfigs.update({
+      where: {
+        guildID: `${interaction.guild?.id}`,
+      },
+      data: {
+        musicIsEnabled: bool,
+      },
+    });
+    interaction.reply({ embeds: [embed] });
+  },
+};
