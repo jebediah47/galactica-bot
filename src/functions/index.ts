4c20c29d70a271563f8195024be0e40701e12e97
Christian Llupo <jebediah47@protonmail.com>
2022-09-09 18:05:56 +0300

    Added refreshConfigCache.ts function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,4 @@
+export { refreshConfigCache } from "./refreshConfigCache";
 export { commaFormatter } from "./commaFormatter";
 export { calculator as calc } from "./calc";
 export { capitalize } from "./capitalize";
