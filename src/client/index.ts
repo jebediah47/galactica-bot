6f2fe75dc403883c3de5e0fdbfe5e93444e325c6
Christian Llupo <jebediah47@protonmail.com>
2022-09-09 18:02:59 +0300

    Updated client/index.ts

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,8 @@
 import { Command, Event, Config, RegisterCommandOptions } from "../interfaces";
+import { PrismaClient, GuildConfigs } from "@prisma/client";
 import { SoundCloudPlugin } from "@distube/soundcloud";
 import { SpotifyPlugin } from "@distube/spotify";
 import * as ConfigJson from "../../config.json";
-import { PrismaClient } from "@prisma/client";
 import { YtDlpPlugin } from "@distube/yt-dlp";
 import { galacticaServer } from "../server";
 import { readdir } from "node:fs/promises";
@@ -15,22 +15,33 @@ import {
 } from "discord.js";
 
 class ExtendedClient extends Client {
+  public configs: Collection<string, GuildConfigs> = new Collection();
   public commands: Collection<string, Command> = new Collection();
+  public slashCommands: ApplicationCommandDataResolvable[] = [];
   public events: Collection<string, Event> = new Collection();
   public prisma: PrismaClient = new PrismaClient();
-  public distube: DisTube | undefined;
+  public distube: DisTube = new DisTube(this, {
+    searchSongs: 0,
+    emitAddSongWhenCreatingQueue: false,
+    emitAddListWhenCreatingQueue: false,
+    plugins: [
+      new YtDlpPlugin({ update: true }),
+      new SpotifyPlugin(),
+      new SoundCloudPlugin(),
+    ],
+    youtubeDL: false,
+  });
   public config: Config = ConfigJson;
   public constructor() {
     super({
       intents: 32767,
     });
   }
-  private async registerCommands({ commands }: RegisterCommandOptions) {
+  public async registerCommands({ commands }: RegisterCommandOptions) {
     await this.application?.commands.set(commands);
   }
   public async init(): Promise<void> {
     this.login(this.config.TOKEN).then();
-    const slashCommands: ApplicationCommandDataResolvable[] = [];
     const command_files = path.join(__dirname, "..", "commands");
     for (const dir of await readdir(command_files)) {
       const commands = (await readdir(`${command_files}/${dir}`)).filter(
@@ -40,14 +51,9 @@ class ExtendedClient extends Client {
       for (const file of commands) {
         const { command } = await import(`${command_files}/${dir}/${file}`);
         this.commands.set(command.name, command);
-        slashCommands.push(command);
+        this.slashCommands.push(command);
       }
     }
-    this.on("ready", () => {
-      this.registerCommands({
-        commands: slashCommands,
-      });
-    });
     const event_files = path.join(__dirname, "..", "events");
     for (const dir of await readdir(event_files)) {
       const events = (await readdir(`${event_files}/${dir}`)).filter(
@@ -64,17 +70,6 @@ class ExtendedClient extends Client {
     if (this.config.SERVER_OPTIONS.ENABLED) {
       galacticaServer(this.config.SERVER_OPTIONS.PORT);
     }
-    this.distube = new DisTube(this, {
-      searchSongs: 0,
-      emitAddSongWhenCreatingQueue: false,
-      emitAddListWhenCreatingQueue: false,
-      plugins: [
-        new YtDlpPlugin({ update: true }),
-        new SpotifyPlugin(),
-        new SoundCloudPlugin(),
-      ],
-      youtubeDL: false,
-    });
   }
 }
 
