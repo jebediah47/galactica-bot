f8e8fd4827491db4942929b791f2af82ce797f5d
Christian Llupo <jebediah47@protonmail.com>
2022-09-09 19:40:34 +0300

    Update refreshConfigCache.ts

--- src/functions/refreshConfigCache.ts
+++ src/functions/refreshConfigCache.ts
@@ -3,5 +3,4 @@ import ExtendedClient from "../client";
 export async function refreshConfigCache(client: ExtendedClient) {
   const freshConfigs = await client.prisma.guildConfigs.findMany();
   freshConfigs.forEach((config) => client.configs.set(config.guildID, config));
-  console.log(freshConfigs);
 }
