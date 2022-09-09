f03c12c88a103e262ebff935d242acbe79f75750
Christian Llupo <jebediah47@protonmail.com>
2022-09-09 18:06:41 +0300

    Update skip.ts

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -5,23 +5,15 @@ export const command: Command = {
   name: "skip",
   description: "Skips a one song in the queue.",
   run: async (client, interaction) => {
-    const db = await client.prisma.guildConfigs.findUnique({
-      where: {
-        guildID: `${interaction.guild?.id}`,
-      },
-      select: {
-        musicIsEnabled: true,
-      },
-    });
-    if (db?.musicIsEnabled) {
-      const queue = client.distube?.getQueue(interaction);
+    if (client.configs.get(interaction.guildId!)?.musicIsEnabled) {
+      const queue = client.distube.getQueue(interaction);
       if (!queue) {
         return interaction.reply({
           content: "There is nothing currently playing in the queue!",
         });
       }
       try {
-        await client.distube?.skip(interaction);
+        await client.distube.skip(interaction);
         const embed = new MessageEmbed()
           .setColor("RANDOM")
           .setTitle("✅ Success!")
