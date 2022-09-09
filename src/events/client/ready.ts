3dbfb233a5751fd7620f6a3d4967cf939d4f92c8
Christian Llupo <jebediah47@protonmail.com>
2022-09-09 18:03:20 +0300

    Updated ready.ts

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -4,14 +4,19 @@ import { Event } from "../../interfaces";
 
 export const event: Event = {
   name: "ready",
-  run: (client) => {
-    if (client.user != null) {
-      process.stdout.write(`We have logged in as ${client.user.tag} \n`);
-      client.user.setActivity(`${client.config.BOT_PRESENCE}`, {
-        type: client.config.BOT_PRESENCE_TYPE as
-          | ExcludeEnum<typeof ActivityTypes, "CUSTOM">
-          | undefined,
-      });
-    }
+  run: async (client) => {
+    client.registerCommands({
+      commands: client.slashCommands,
+    });
+    const guildConfigs = await client.prisma.guildConfigs.findMany();
+    guildConfigs.forEach((config) =>
+      client.configs.set(config.guildID, config)
+    );
+    process.stdout.write(`We have logged in as ${client.user!.tag} \n`);
+    client.user!.setActivity(`${client.config.BOT_PRESENCE}`, {
+      type: client.config.BOT_PRESENCE_TYPE as
+        | ExcludeEnum<typeof ActivityTypes, "CUSTOM">
+        | undefined,
+    });
   },
 };
