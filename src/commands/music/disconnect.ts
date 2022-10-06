3a8c66f58974947300b3f35dffe174b472975c0f
Christian Llupo <jebediah47@protonmail.com>
2022-10-06 20:30:37 +0300

    Updated disconnect.ts to v14

--- src/commands/music/disconnect.ts
+++ src/commands/music/disconnect.ts
@@ -1,17 +1,18 @@
+import { GuildIdResolvable } from "distube";
 import { Command } from "../../interfaces";
-import { MessageEmbed } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 
 export const command: Command = {
   name: "disconnect",
   description: "Disconnects the bot from current voice channel.",
   run: async (client, interaction) => {
     if (client.configs.get(interaction.guildId!)?.musicIsEnabled) {
-      if (client.distube.voices.has(interaction)) {
-        client.distube.voices.leave(interaction);
+      if (client.distube.voices.has(interaction as GuildIdResolvable)) {
+        client.distube.voices.leave(interaction as GuildIdResolvable);
         await interaction.reply({ content: "👋" });
       } else {
-        const noVoiceChannel = new MessageEmbed()
-          .setColor("RANDOM")
+        const noVoiceChannel = new EmbedBuilder()
+          .setColor("Random")
           .setTitle("❌ Error!")
           .setDescription("The bot is not connected to any voice-channel!")
           .setTimestamp();
