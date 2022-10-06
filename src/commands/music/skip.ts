769b21a54bab905d6a2f2102e836daa64b6fb27c
Christian Llupo <jebediah47@protonmail.com>
2022-10-06 20:32:01 +0300

    Updated skip.ts to v14

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -1,28 +1,29 @@
+import { GuildIdResolvable } from "distube";
 import { Command } from "../../interfaces";
-import { MessageEmbed } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 
 export const command: Command = {
   name: "skip",
   description: "Skips a one song in the queue.",
   run: async (client, interaction) => {
     if (client.configs.get(interaction.guildId!)?.musicIsEnabled) {
-      const queue = client.distube.getQueue(interaction);
+      const queue = client.distube.getQueue(interaction as GuildIdResolvable);
       if (!queue) {
         return interaction.reply({
           content: "There is nothing currently playing in the queue!",
         });
       }
       try {
-        await client.distube.skip(interaction);
-        const embed = new MessageEmbed()
-          .setColor("RANDOM")
+        await client.distube.skip(interaction as GuildIdResolvable);
+        const embed = new EmbedBuilder()
+          .setColor("Random")
           .setTitle("✅ Success!")
           .setDescription("Skipped song!")
           .setTimestamp();
         await interaction.reply({ embeds: [embed] });
       } catch (err) {
-        const errEmbed = new MessageEmbed()
-          .setColor("RANDOM")
+        const errEmbed = new EmbedBuilder()
+          .setColor("Random")
           .setTitle("❌ Error!")
           .setDescription(`${err}`)
           .setTimestamp();
