de79e2894ad75b2a830ab15e1c40ef5e207e9a11
Christian Llupo <jebediah47@protonmail.com>
2022-10-06 20:32:13 +0300

    Updated volume.ts to v14

--- src/commands/music/volume.ts
+++ src/commands/music/volume.ts
@@ -1,6 +1,7 @@
 import { Command } from "../../interfaces";
-import { MessageEmbed } from "discord.js";
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 import { isInteger } from "mathjs";
+import { GuildIdResolvable } from "distube";
 
 export const command: Command = {
   name: "volume",
@@ -9,20 +10,20 @@ export const command: Command = {
     {
       name: "vol",
       description: "A value from 0 to 100",
-      type: "NUMBER",
+      type: ApplicationCommandOptionType.Number,
       required: true,
     },
   ],
   run: async (client, interaction, args) => {
     if (client.configs.get(interaction.guildId!)?.musicIsEnabled) {
-      const queue = client.distube.getQueue(interaction);
+      const queue = client.distube.getQueue(interaction as GuildIdResolvable);
       if (!queue) {
         return interaction.reply({
           content: "There is nothing currently playing in the queue!",
         });
       }
-      const errEmbed = new MessageEmbed()
-        .setColor("RANDOM")
+      const errEmbed = new EmbedBuilder()
+        .setColor("Random")
         .setTitle("Notice!")
         .setDescription(
           "You **must** enter a valid number from the range 1 - 100"
@@ -32,22 +33,22 @@ export const command: Command = {
       if (!isInteger(volume)) return interaction.reply({ embeds: [errEmbed] });
 
       if (volume > 100) {
-        const errEmbed2 = new MessageEmbed()
-          .setColor("RANDOM")
+        const errEmbed2 = new EmbedBuilder()
+          .setColor("Random")
           .setTitle("Volume cannot be over 100%")
           .setTimestamp();
         return interaction.reply({ embeds: [errEmbed2] });
       }
       try {
         queue.setVolume(volume);
-        const volume_embed = new MessageEmbed()
-          .setColor("RANDOM")
+        const volume_embed = new EmbedBuilder()
+          .setColor("Random")
           .setTitle(`🔊Set volume to \`${volume}\`%`)
           .setTimestamp();
         await interaction.reply({ embeds: [volume_embed] });
       } catch (err) {
-        const errEmbed = new MessageEmbed()
-          .setColor("RANDOM")
+        const errEmbed = new EmbedBuilder()
+          .setColor("Random")
           .setTitle("❌ Error!")
           .setDescription(`${err}`)
           .setTimestamp();
