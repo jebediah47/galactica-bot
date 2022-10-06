32460c0c74aa8a9b6c124b41eff976a32883cc05
Christian Llupo <jebediah47@protonmail.com>
2022-10-06 20:31:15 +0300

    Updated resume.ts to v14

--- src/commands/music/resume.ts
+++ src/commands/music/resume.ts
@@ -1,12 +1,13 @@
-import { MessageEmbed } from "discord.js";
+import { GuildIdResolvable } from "distube";
 import { Command } from "../../interfaces";
+import { EmbedBuilder } from "discord.js";
 
 export const command: Command = {
   name: "resume",
   description: "Resumes the queue.",
   run: async (client, interaction) => {
     if (client.configs.get(interaction.guildId!)?.musicIsEnabled) {
-      const queue = client.distube.getQueue(interaction);
+      const queue = client.distube.getQueue(interaction as GuildIdResolvable);
       if (!queue) {
         return interaction.reply({
           content: "There is nothing currently playing in the queue!",
@@ -16,8 +17,8 @@ export const command: Command = {
         queue.resume();
         await interaction.reply({ content: "The queue has been resumed." });
       } catch (err) {
-        const errEmbed = new MessageEmbed()
-          .setColor("RANDOM")
+        const errEmbed = new EmbedBuilder()
+          .setColor("Random")
           .setTitle("❌ Error!")
           .setDescription(`${err}`)
           .setTimestamp();
