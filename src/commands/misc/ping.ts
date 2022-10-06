c401fe7b06340b1eae8030f7144f807ac60f99ca
Christian Llupo <jebediah47@protonmail.com>
2022-10-06 20:26:06 +0300

    Updated ping.ts to v14

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -1,13 +1,13 @@
 import { Command } from "../../interfaces";
-import { MessageEmbed } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 import { stdout } from "process";
 
 export const command: Command = {
   name: "ping",
   description: "Replies with the bot's ping.",
   run: async (client, interaction) => {
-    const embed = new MessageEmbed()
-      .setColor("RANDOM")
+    const embed = new EmbedBuilder()
+      .setColor("Random")
       .setDescription(`The bot's ping is **${client.ws.ping}**`)
       .setTimestamp();
     await interaction
