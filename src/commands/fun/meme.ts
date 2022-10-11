b5071f2affe364537b16ff82205ab6387e38f18a
Christian Llupo <jebediah47@protonmail.com>
2022-10-11 21:09:17 +0300

    Improved meme.ts this time

--- src/commands/fun/meme.ts
+++ src/commands/fun/meme.ts
@@ -2,6 +2,15 @@ import { Command } from "../../interfaces";
 import { EmbedBuilder } from "discord.js";
 import axios from "axios";
 
+function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomPost(posts: string | any[]) {
+  const randomIndex = randomInt(0, posts.length);
+  return posts[randomIndex].data;
+}
+
 export const command: Command = {
   name: "meme",
   description: "Sends you an EPIC meme!",
@@ -14,28 +23,20 @@ export const command: Command = {
       "r/PewdiepieSubmissions",
       "r/MemeEconomy",
     ];
-    function randomInt(min: number, max: number) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
     const randomIndex = randomInt(0, subReddits.length);
-    function getRandomPost(posts: any) {
-      const randomIndex = randomInt(0, posts.length);
-      return posts[randomIndex].data;
-    }
+
     await axios
       .get(`https://reddit.com/${subReddits[randomIndex]}/.json`)
-      .then(async (resp) => {
-        const {
-          title,
-          url,
-          subreddit_name_prefixed: subreddit,
-        } = getRandomPost(resp.data.data.children);
+      .then(async (response) => {
+        const { url, ups, title, permalink, num_comments } = getRandomPost(
+          response.data.data.children
+        );
         const meme = new EmbedBuilder()
           .setTitle(`${title}`)
-          .setURL(`${url}`)
+          .setURL(`https://reddit.com${permalink}`)
           .setColor("Random")
           .setImage(`${url}`)
-          .setFooter({ text: `From ${subreddit}` });
+          .setFooter({ text: `👍 ${ups} 💬 ${num_comments}` });
 
         await interaction.reply({ embeds: [meme] });
       });
