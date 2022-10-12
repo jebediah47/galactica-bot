9cb643371d06f8a73de0cfc8aa6246339d42e3b4
Christian Llupo <jebediah47@protonmail.com>
2022-10-12 17:25:05 +0300

    Added database checking on queue.ts

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -5,39 +5,41 @@ import { Command } from "../../interfaces";
 
 export const command: Command = {
   name: "queue",
-  description: "Shows the music queue",
+  description: "Shows the music queue.",
   run: async (client, interaction) => {
     const queue = client.distube.getQueue(interaction as GuildIdResolvable);
-    if (!queue)
-      return interaction.reply({
-        content: "There is nothing currently playing in the queue!",
+    if (client.configs.get(interaction.guildId!)?.musicIsEnabled) {
+      if (!queue)
+        return interaction.reply({
+          content: "There is nothing currently playing in the queue!",
+        });
+      const q: string = queue.songs
+        .map(
+          (song, i) =>
+            `${i === 0 ? "🔊 **Playing:**" : `**${i}.**`} ${song.name} - \`${
+              song.formattedDuration
+            }\``
+        )
+        .join("\n");
+
+      const embeds = [];
+      for (let i = 0; i < q.length; i += 2048) {
+        const toSend = q.substring(i, Math.min(q.length, i + 2048));
+        embeds.push(
+          new EmbedBuilder()
+            .setColor("Random")
+            .setTitle("🎵 The server's music queue")
+            .setDescription(toSend)
+            .setTimestamp()
+        );
+      }
+
+      await pagination({
+        embeds: embeds as unknown as Embed[],
+        author: interaction.member.user,
+        interaction: interaction,
+        time: 30000,
       });
-    const q: string = queue.songs
-      .map(
-        (song, i) =>
-          `${i === 0 ? "🔊 **Playing:**" : `**${i}.**`} ${song.name} - \`${
-            song.formattedDuration
-          }\``
-      )
-      .join("\n");
-
-    const embeds = [];
-    for (let i = 0; i < q.length; i += 2048) {
-      const toSend = q.substring(i, Math.min(q.length, i + 2048));
-      embeds.push(
-        new EmbedBuilder()
-          .setColor("Random")
-          .setTitle("🎵 The server's music queue")
-          .setDescription(toSend)
-          .setTimestamp()
-      );
     }
-
-    await pagination({
-      embeds: embeds as unknown as Embed[],
-      author: interaction.member.user,
-      interaction: interaction,
-      time: 30000,
-    });
   },
 };
