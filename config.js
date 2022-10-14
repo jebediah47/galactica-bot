05b56345fec50349abfe7991aaa7e35c5d315d43
Christian Llupo <jebediah47@protonmail.com>
2022-10-14 17:03:50 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+/* eslint-env es6 */
+const { ActivityType } = require("discord.js");
+
+module.exports.config = {
+  BOT_PRESENCE: "YOUR_BOT_PRESENCE",
+  BOT_PRESENCE_TYPE: ActivityType.Watching,
+};
