ca8572c25bd57886276a85cfdb7761e35490a5d8
Christian Llupo <jebediah47@protonmail.com>
2022-12-07 20:02:49 +0200

    Fixed error squiggle on tictactoe.ts

--- src/commands/fun/tictactoe.ts
+++ src/commands/fun/tictactoe.ts
@@ -1,11 +1,12 @@
-import TicTacToe from "discord-tictactoe";
+import { ChatInputCommandInteraction } from "discord.js";
 import { Command } from "../../interfaces";
+import TicTacToe from "discord-tictactoe";
 
 export const command: Command = {
   name: "tictactoe",
   description: "Creates a Tic-Tac-Toe game with an AI as the opponent.",
   run: (client, interaction) => {
     const game = new TicTacToe({ language: "en" });
-    game.handleInteraction(interaction);
+    game.handleInteraction(interaction as ChatInputCommandInteraction);
   },
 };
