2998f58ef379f395462fabcf7b20eed8e7956d24
Christian Llupo <jebediah47@protonmail.com>
2024-11-03 13:06:17 +0200

    Create PresenceManager.ts

---
+++ src/classes/PresenceManager.ts
@@ -0,0 +1,19 @@
+import { ActivityType } from "discord-api-types/payloads/v10";
+import ExtendedClient from "@/client";
+
+export class PresenceManager {
+	private _client: ExtendedClient;
+
+	public constructor(client: ExtendedClient) {
+		this._client = client;
+	}
+
+	public setPresence(presence: string, presenceType: ActivityType): void {
+		this._client.user?.setActivity(presence, {
+			type: presenceType,
+		});
+		this._client.logger.info(
+			`Presence set to ${presence} with type ${presenceType}`,
+		);
+	}
+}
