f318f6d6698cc749496a61bc51f73dcfb508efe4
Christian Llupo <jebediah47@protonmail.com>
2024-11-03 13:01:25 +0200

    Remove previous configuration method

--- config.ts
+++
@@ -1,6 +0,0 @@
-import { ActivityType } from "discord.js";
-
-export const config = {
-  BOT_PRESENCE: "Example",
-  BOT_PRESENCE_TYPE: ActivityType.Watching,
-};
--- src/interfaces/Config.ts
+++
@@ -1,6 +0,0 @@
-import { ActivityType } from "discord.js";
-
-export interface Config {
-  BOT_PRESENCE: string;
-  BOT_PRESENCE_TYPE: ActivityType;
-}
