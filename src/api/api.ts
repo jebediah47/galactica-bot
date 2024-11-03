aa15f1b67f99c09fce3b72e1635773a886bafa1a
Christian Llupo <jebediah47@protonmail.com>
2024-11-03 20:00:17 +0200

    Added Bearer authorization
    Bearer is set in the GALACTICA_API_BEARER environment variable

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import { Elysia } from "elysia";
 import { apollo } from "@elysiajs/apollo";
+import { bearer } from "@elysiajs/bearer";
 import ExtendedClient from "@/client";
 import resolvers from "@/api/resolvers";
 import { schema } from "@/api/schema";
@@ -13,12 +14,41 @@ export class GalacticaBotAPI {
   }
 
   public async start() {
+    if (!process.env.GALACTICA_API_BEARER) {
+      this._client.logger.fatal(
+        "No GALACTICA_API_BEARER provided in the environment, shutting down...",
+      );
+      process.exit(1);
+    }
+
+    this._api.use(bearer()).onBeforeHandle(({ bearer, set, request }) => {
+      const url = new URL(request.url);
+      if (
+        process.env.NODE_ENV === "development" &&
+        url.pathname.startsWith("/api/graphql")
+      ) {
+        return;
+      }
+
+      const isAuthorized = bearer === process.env.GALACTICA_API_BEARER;
+
+      if (!isAuthorized || !bearer) {
+        set.status = 400;
+        set.headers["WWW-Authenticate"] =
+          `Bearer realm='sign', error="invalid_request"`;
+        this._client.logger.error("Unauthorized");
+        throw new Error("Unauthorized");
+      }
+    });
+
     this._api.use(
       apollo({
         typeDefs: schema,
         resolvers: resolvers(this._client),
       }),
     );
+
+    this._api.get("/", "If you're seeing this you're authorized");
 
     const API_PORT = process.env.GALACTICA_API_PORT || 3000;
 
--- src/environment.d.ts
+++ src/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       GALACTICA_TOKEN: string;
+      GALACTICA_API_BEARER: string;
       GALACTICA_API_PORT?: string | number;
     }
   }
