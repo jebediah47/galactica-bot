9d81200bbd368d1fdaafaa09f735ce5221ebe845
Christian Llupo <jebediah47@protonmail.com>
2024-11-03 13:05:08 +0200

    Create ConfigManager.ts

---
+++ src/classes/ConfigManager.ts
@@ -0,0 +1,37 @@
+import Conf, { Schema } from "conf";
+
+export class ConfigManager {
+	private _config: Conf;
+
+	private schema: Schema<Record<string, unknown>> = {
+		botPresence: {
+			type: "string",
+			default: "for commands!",
+		},
+		botPresenceType: {
+			type: "number",
+			maximum: 6,
+			minimum: 0,
+			default: 3,
+		},
+	};
+
+	public constructor() {
+		this._config = new Conf({
+			projectName: "galactica-bot",
+			schema: this.schema,
+		});
+	}
+
+	public get(key: string): any {
+		return this._config.get(key);
+	}
+
+	public set(key: string, value: string | number): void {
+		this._config.set(key, value);
+	}
+
+	public delete(key: string): void {
+		this._config.delete(key);
+	}
+}
