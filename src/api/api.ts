5ee7ed56b03c11aec41df5743e155eed7c6fdadf
Christian Llupo <jebediah47@protonmail.com>
2024-11-30 14:30:35 +0200

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,4 @@
+import { authPlugin } from "@/api/auth/plugin"
 import { createAuthRoutes } from "@/api/auth/routes"
 import type ExtendedClient from "@/client"
 import { GALACTICA_API_PORT } from "@/constants"
@@ -13,47 +14,43 @@ export class GalacticaBotAPI {
 
   public async start() {
     this._api.use(createAuthRoutes(this._client))
+    this._api.get("/config/presence", () => {
+      return {
+        presence: this._client.configManager.get("botPresence"),
+        presence_type: this._client.configManager.get("botPresenceType"),
+      }
+    })
+    this._api.use(authPlugin(this._client))
     this._api.post(
       "/config/presence",
       ({ body }) => {
-        const { bot_presence, bot_presence_type } = body
+        const { presence, presence_type } = body
 
-        if (
-          typeof bot_presence_type === "number" &&
-          (bot_presence_type < 0 || bot_presence_type > 6)
-        ) {
-          this._client.logger.error(
-            "bot_presence_type must be >= 0 and <= 6. Not updating presence.",
-          )
-        } else {
-          this._client.configManager.set("botPresence", bot_presence)
-          this._client.configManager.set("botPresenceType", bot_presence_type)
+        this._client.configManager.set("botPresence", presence)
+        this._client.configManager.set("botPresenceType", presence_type)
 
-          this._client.presenceManager.setPresence(
-            this._client.configManager.get("botPresence"),
-            this._client.configManager.get("botPresenceType"),
-          )
-        }
+        this._client.presenceManager.setPresence(
+          this._client.configManager.get("botPresence"),
+          this._client.configManager.get("botPresenceType"),
+        )
 
         return {
-          bot_presence: this._client.configManager.get("botPresence"),
-          bot_presence_type: this._client.configManager.get("botPresenceType"),
+          presence: this._client.configManager.get("botPresence"),
+          presence_type: this._client.configManager.get("botPresenceType"),
         }
       },
       {
         body: t.Object({
-          bot_presence: t.String(),
-          bot_presence_type: t.Number(),
+          presence: t.String({ minLength: 1, maxLength: 96 }),
+          presence_type: t.Number({ minimum: 0, maximum: 6 }),
         }),
       },
     )
 
     this._api.get("/", "If you're seeing this you're authorized")
 
-    const API_PORT = GALACTICA_API_PORT
-
-    this._api.listen(API_PORT, () => {
-      this._client.logger.info(`API listening on port ${API_PORT}`)
+    this._api.listen(GALACTICA_API_PORT, () => {
+      this._client.logger.info(`API listening on port ${GALACTICA_API_PORT}`)
     })
   }
 }
