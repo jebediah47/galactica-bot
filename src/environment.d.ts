fdde8f39fe4186fbe7dd3ffca0882e485992e7d4
Christian Llupo <jebediah47@protonmail.com>
2024-11-30 13:09:24 +0200

    Update environment.d.ts

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -2,8 +2,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       GALACTICA_TOKEN: string
-      GALACTICA_API_BEARER: string
       GALACTICA_API_PORT?: string | number
+      JWT_SECRET: string
     }
   }
 }
