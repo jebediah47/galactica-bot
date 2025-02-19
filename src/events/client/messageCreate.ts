81e19957a175eba13d28020641f02e92d3af01d9
Christian Llupo <jebediah47@protonmail.com>
2025-02-19 20:28:11 +0200

    Update messageCreate.ts

--- src/events/client/messageCreate.ts
+++ src/events/client/messageCreate.ts
@@ -6,6 +6,5 @@ export const event: Event<"messageCreate"> = {
   run: async (client, message: Message) => {
     if (!message.inGuild() || message.author.bot) return
     await client.levels.giveXp(message)
-    process.stdout.write(`\r${message.author.tag} has sent a message.\n`)
   },
 }
